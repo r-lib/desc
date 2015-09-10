601896db9bf42da3635db5ff94452ebe45d09749
Gabor Csardi <csardi.gabor@gmail.com>
2015-09-10 12:03:09 +0200

    Keep all white space when reading DCF files

--- R/read.R
+++ R/read.R
@@ -1,4 +1,13 @@
 
 ## TODO: handle empty files
 
-read_dcf <- function(...) read.dcf(...)
+read_dcf <- function(file, ...) {
+  lines <- readLines(file)
+
+  con <- textConnection(lines, local = TRUE)
+  fields <- colnames(read.dcf(con))
+  close(con)
+
+  con <- textConnection(lines, local = TRUE)
+  read.dcf(con, keep.white = fields)
+}
