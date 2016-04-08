0d78dfda7bffe817ff2e9f5a5571f268318ae232
Kirill Müller <kirill.mueller@ivt.baug.ethz.ch>
2016-04-08 17:59:46 +0200

    VignetteBuilder is dependency field

--- tests/testthat/test-deps.R
+++ tests/testthat/test-deps.R
@@ -6,9 +6,9 @@ test_that("get_deps", {
 
   res <- data.frame(
     stringsAsFactors = FALSE,
-    type = c("Imports", "Suggests"),
-    package = c("R6", "testthat"),
-    version = c("*", "*")
+    type = c("Imports", "Suggests", "VignetteBuilder"),
+    package = c("R6", "testthat", "knitr"),
+    version = c("*", "*", "*")
   )
 
   expect_equal(desc$get_deps(), res)
@@ -22,9 +22,9 @@ test_that("set_dep", {
 
   res <- data.frame(
     stringsAsFactors = FALSE,
-    type = c("Imports", "Imports", "Suggests"),
-    package = c("R6", "igraph", "testthat"),
-    version = c("*", "*", "*")
+    type = c("Imports", "Imports", "Suggests", "VignetteBuilder"),
+    package = c("R6", "igraph", "testthat", "knitr"),
+    version = c("*", "*", "*", "*")
   )
   expect_equal(desc$get_deps(), res)
 
@@ -32,9 +32,9 @@ test_that("set_dep", {
 
   res <- data.frame(
     stringsAsFactors = FALSE,
-    type = c("Imports", "Imports", "Suggests"),
-    package = c("R6", "igraph", "testthat"),
-    version = c("*", ">= 1.0.0", "*")
+    type = c("Imports", "Imports", "Suggests", "VignetteBuilder"),
+    package = c("R6", "igraph", "testthat", "knitr"),
+    version = c("*", ">= 1.0.0", "*", "*")
   )
 
   expect_equal(desc$get_deps(), res)
@@ -43,9 +43,9 @@ test_that("set_dep", {
 
   res <- data.frame(
     stringsAsFactors = FALSE,
-    type = c("Imports", "Imports", "Suggests", "Depends"),
-    package = c("R6", "igraph", "testthat", "igraph"),
-    version = c("*", ">= 1.0.0", "*", ">= 1.0.0")
+    type = c("Imports", "Imports", "Suggests", "VignetteBuilder", "Depends"),
+    package = c("R6", "igraph", "testthat", "knitr", "igraph"),
+    version = c("*", ">= 1.0.0", "*", "*", ">= 1.0.0")
   )
 
   expect_equal(desc$get_deps(), res)
@@ -76,9 +76,9 @@ test_that("del_dep", {
 
   res <- data.frame(
     stringsAsFactors = FALSE,
-    type = c("Imports", "Suggests"),
-    package = c("R6", "testthat"),
-    version = c("*", "*")
+    type = c("Imports", "Suggests", "VignetteBuilder"),
+    package = c("R6", "testthat", "knitr"),
+    version = c("*", "*", "*")
   )
 
   expect_equal(desc$get_deps(), res)
@@ -91,9 +91,9 @@ test_that("del_dep", {
 
   res <- data.frame(
     stringsAsFactors = FALSE,
-    type = c("Imports", "Suggests", "Depends"),
-    package = c("R6", "testthat", "igraph"),
-    version = c("*", "*", ">= 1.0.0")
+    type = c("Imports", "Suggests", "VignetteBuilder", "Depends"),
+    package = c("R6", "testthat", "knitr", "igraph"),
+    version = c("*", "*", "*", ">= 1.0.0")
   )
 
   expect_equal(desc$get_deps(), res)
