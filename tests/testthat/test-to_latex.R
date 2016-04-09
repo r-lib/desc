6dfca04bf1a7d032d3472eab313852b15eee591f
Kirill Müller <kirill.mueller@ivt.baug.ethz.ch>
2016-04-09 16:56:17 +0200

    actually test multiple URLs

--- tests/testthat/test-to_latex.R
+++ tests/testthat/test-to_latex.R
@@ -8,7 +8,7 @@ test_that("Test expected LaTeX output", {
            Title = "Must be in Title Case",
            Description = "Words not in the dictionary must be 'quoted'. \"Double quotes\" can also be used, as well as \\_{}[]^$#%. Must end with a full stop.",
            License = "CC-BY-SA",
-           URL = "http://somewhere.io",
+           URL = "http://somewhere.io, http://somewhereel.se",
            BugReports = "http://somewhere.io/trac")
 
   expect_output_file(cat(desc$to_latex()), "output/to_latex.tex", update = TRUE)
