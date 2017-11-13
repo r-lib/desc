d6b7fb34180bc696e2bdf896e2c4b717a7106600
Gábor Csárdi <csardi.gabor@gmail.com>
2017-11-13 23:44:03 +0000

    Fix warnings on non-native encodings

--- R/read.R
+++ R/read.R
@@ -5,11 +5,6 @@ portable_dcf_encodings <- function() c("latin1", "latin2", "UTF-8")
 
 read_dcf <- function(file) {
   lines <- readLines(file)
-  no_tws_fields <- sub(
-    ":$",
-    "",
-    grep("^[^\\s]+:$", lines, perl = TRUE, value = TRUE)
-  )
 
   con <- textConnection(lines, local = TRUE)
   fields <- colnames(read.dcf(con))
@@ -29,6 +24,13 @@ read_dcf <- function(file) {
   }
 
   res <- fix_dcf_encoding_read(res)
+
+  lines <- enc2utf8(lines)
+  no_tws_fields <- sub(
+    ":$",
+    "",
+    grep("^[^\\s]+:$", lines, perl = TRUE, value = TRUE)
+  )
 
   notws <- res[1, match(no_tws_fields, fields)]
 
