8375666c9f7d01fa2e76fa86bbb8fd27c34091b8
Lluís Revilla <lluis.revilla@gmail.com>
2018-07-29 11:10:13 +0200

    Correct the tests for the ordered dependencies

--- tests/testthat/test-repair.R
+++ tests/testthat/test-repair.R
@@ -14,7 +14,7 @@ test_that("normalization", {
   expect_equal(sort(before_fields), sort(after_fields))
   expect_lt(match("Imports", after_fields), match("Imports", before_fields))
   expect_gt(match("LazyData", after_fields), match("LazyData", before_fields))
-  expect_equal(after_data[["Imports"]], "\n    foo,\n    bar,\n    foobar")
+  expect_equal(after_data[["Imports"]], "\n    bar,\n    foo,\n    foobar")
 
 })
 
@@ -30,7 +30,7 @@ test_that("reformatting", {
   after_data <- desc$get(after_fields)
 
   expect_equal(before_fields, after_fields)
-  expect_equal(after_data[["Imports"]], "\n    foo,\n    bar,\n    foobar")
+  expect_equal(after_data[["Imports"]], "\n    bar,\n    foo,\n    foobar")
 
 })
 
