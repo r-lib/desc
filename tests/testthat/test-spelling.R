272064ff888d052e7e006b4ef5882b59e63665ed
Gábor Csárdi <csardi.gabor@gmail.com>
2021-04-28 19:10:43 +0200

    Run spell check from the testthat test suite

---
+++ tests/testthat/test-spelling.R
@@ -0,0 +1,32 @@
+
+test_package_root <- function() {
+  x <- tryCatch(
+    rprojroot::find_package_root_file(),
+    error = function(e) NULL)
+
+  if (!is.null(x)) return(x)
+
+  pkg <- testthat::testing_package()
+  x <- tryCatch(
+    rprojroot::find_package_root_file(
+      path = file.path("..", "..", "00_pkg_src", pkg)),
+    error = function(e) NULL)
+
+  if (!is.null(x)) return(x)
+
+  stop("Cannot find package root")
+}
+
+test_that("spelling", {
+  skip_on_cran()
+  pkgroot <- test_package_root()
+  err <- spelling::spell_check_package(pkgroot)
+  num_spelling_errors <- nrow(err)
+  expect_true(
+    num_spelling_errors == 0,
+    info = paste(
+      c("\nSpelling errors:", capture.output(err)),
+      collapse = "\n"
+    )
+  )
+})
