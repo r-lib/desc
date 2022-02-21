43047ea3e3be65f4f703d8f78d495d325dbcf03d
Gábor Csárdi <csardi.gabor@gmail.com>
2022-02-21 15:27:07 +0100

    More deparse fixes
    Cannot use `iconv()`, need to do this manually.

--- R/utils.R
+++ R/utils.R
@@ -137,6 +137,12 @@ parse_full_name <- function(x) {
 # if we convert them to <U+xxxx> with iconv, and then convert back the
 # <U+xxxx> strings after the deparsing.
 #
+# We cannot do the conversion to <U+> with iconv() because it only supports
+# this kind of escaping from R 4.0.x.
+#
+# We also need to do the conversion on UTF-8 systems, because `deparse()`
+# on older R versions escapes some Unicode characters.
+#
 # This function only works for character vectors, obviously. Related:
 # https://stat.ethz.ch/pipermail/r-devel/2022-February/081485.html
 
@@ -148,20 +154,23 @@ fixed_deparse1 <- function(x, ...) {
 }
 
 unicode_encode <- function(x) {
-  x <- enc2utf8(x)
-  if (! l10n_info()$`UTF-8`) {
-    x <- iconv(x, "UTF-8", "ASCII", sub = "Unicode")
-  }
+  x[] <- vapply(x, unicode_encode1, character(1), USE.NAMES = FALSE)
   x
 }
 
+unicode_encode1 <- function(x) {
+  x <- enc2utf8(x)
+  nm <- utf8ToInt(x)
+  lt <- intToUtf8(nm, multiple = TRUE)
+  lt[nm > 127] <- paste0("<U+", as.hexmode(nm[nm > 127]), ">")
+  paste(lt, collapse = "")
+}
+
 unicode_decode <- function(x) {
-  if (! l10n_info()$`UTF-8`) {
-    mch <- gregexpr("<U\\+([0-9a-fA-F]+)>", x, perl = TRUE)
-    uni <- regmatches(x, mch)
-    rep <- lapply(uni, parse_escaped_unicode)
-    regmatches(x, mch) <- rep
-  }
+  mch <- gregexpr("<U\\+([0-9a-fA-F]+)>", x, perl = TRUE)
+  uni <- regmatches(x, mch)
+  rep <- lapply(uni, parse_escaped_unicode)
+  regmatches(x, mch) <- rep
   x <- enc2utf8(x)
   Encoding(x) <- "UTF-8"
   x
