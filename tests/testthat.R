200504a65cdd2a1fd16638b1a62e57e10cbac029
Gábor Csárdi <csardi.gabor@gmail.com>
2022-03-06 12:14:26 +0100

    Do not run tests on CRAN

--- tests/testthat.R
+++ tests/testthat.R
@@ -1,4 +1,6 @@
-library(testthat)
-library(desc)
 
-test_check("desc", reporter = "summary")
+if (Sys.getenv("NOT_CRAN") == "true") {
+  library(testthat)
+  library(desc)
+  test_check("desc", reporter = "summary")
+}
