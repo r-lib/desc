77ebaa5023a1aaba888d5f7d3b4266217efcd5f1
Gábor Csárdi <csardi.gabor@gmail.com>
2024-08-28 16:35:36 +0200

    Fix code formatting

--- tests/testthat/test-built.R
+++ tests/testthat/test-built.R
@@ -19,9 +19,11 @@ test_that("corrupted Build field", {
 test_that("get built works with multiples lines", {
   expect_identical(
     desc_get_built(test_path("D16")),
-    list(R = R_system_version("3.4.1"),
-         Platform = "x86_64-apple-darwin15.6.0",
-         Date = "2017-09-14 20:30:19 UTC",
-         OStype = "unix")
+    list(
+      R = R_system_version("3.4.1"),
+      Platform = "x86_64-apple-darwin15.6.0",
+      Date = "2017-09-14 20:30:19 UTC",
+      OStype = "unix"
+    )
   )
 })
